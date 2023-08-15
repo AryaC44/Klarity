1ccafbaacfb47b416b5430f4a9e7892303976f88
Arya Chowdhury <acschool.445@gmail.com>
2023-08-15 14:07:07 -0400

    Initial Commit

---
+++ Klarity/KlarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlarityApp.swift
+//  Klarity
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct KlarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
